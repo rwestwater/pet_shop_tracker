2f17fc7a2f3385722724a10ce30d2b094fb2af0d
Rachel Westwater <rachel-westwater@hotmail.com>
2018-04-13 11:31:49 +0100

    Third test completed successfully

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,3 +1,11 @@
 def pet_shop_name(shop)
   return shop[:name]
 end
+
+def total_cash(shop)
+  return shop[:admin][:total_cash]
+end
+
+def add_or_remove_cash(shop, total)
+  return shop[:admin][:total_cash] += total
+end
