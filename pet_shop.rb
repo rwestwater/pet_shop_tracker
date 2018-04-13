6485bdd072edbe912aa758afdf98dbc0e3c1920f
Rachel Westwater <rachel-westwater@hotmail.com>
2018-04-13 14:16:31 +0100

    10th test completed successfully

--- pet_shop.rb
+++ pet_shop.rb
@@ -21,3 +21,20 @@ end
 def stock_count(count)
   count[:pets].length
 end
+
+def pets_by_breed(pet, type)
+ type_breed =[]
+  for pet in pet[:pets]
+    type_breed.push(type) if pet[:breed] == type
+  end
+  return type_breed
+end
+
+def find_pet_by_name(hash, name)
+  for pets in hash[:pets]
+    if (pets[:name] == name)
+      return pets
+  end
+end
+return nil
+end
